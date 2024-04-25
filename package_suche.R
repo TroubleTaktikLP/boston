f683796d294e8409cf7172310c328eb031d80d26
Jannes Teupen <jannes.teupen@outlook.de>
2024-04-25 20:25:30 +0200

    first data

---
+++ package_suche.R
@@ -0,0 +1,20 @@
+library("faraway")
+View(nepali) # Viele verschiedene Daten, 1000 Entries, bisschen weird ID
+?nepali
+summary(nepali)
+install.packages("MASS")
+library("MASS")
+View(quine) # Recht simpel, aber machbar
+?quine
+View(Boston) # Geiler Datensatz aber catcht nicht wirklich
+?Boston
+View(survey)
+
+#unsere Favs waren neapli und Survey aber Survey haben wir jetzt genommen
+#UScereal kann man auch machen
+
+View(Boston)
+summary(Boston)
+?Boston
+function(v) { uniqv <- unique(v)}
+function(v)
